14e913608c2ada00b0942c0b29aa027cba1df31d
Rowgrid <156215225+Rowgrid@users.noreply.github.com>
2024-07-23 21:21:23 +0530

    added domain name App.js

--- Server/App.js
+++ Server/App.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://www.rowgrid.in",
     credentials: true,
   })
 );
